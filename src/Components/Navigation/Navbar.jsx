a7c0e818ecc6c5ccbc36a3bb49139da74a535fbe
AradhyaSharma31 <110249154+AradhyaSharma31@users.noreply.github.com>
2025-01-01 03:33:32 +1100

    minflash

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -161,7 +161,6 @@ const Navbar = () => {
                     onClick={() => {
                       updateDeckId(d.DeckId);
                       navigate(`/user/explore/${d.DeckId}`);
-                      window.location.reload(true);
                     }}
                     className="overflow-hidden flex flex-row items-center gap-5 w-full h-14 bg-[#042443] text-white px-3 py-2 mb-2 rounded-md hover:bg-[#233559] transition duration-200 cursor-pointer"
                   >
