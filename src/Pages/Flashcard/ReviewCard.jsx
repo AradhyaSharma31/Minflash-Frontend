2b39271b80d91592c616bd8c18ca93f8d8f25ad9
AradhyaSharma31 <110249154+AradhyaSharma31@users.noreply.github.com>
2025-02-02 19:02:11 +0530

    bug fixes and updates

--- src/Pages/Flashcard/ReviewCard.jsx
+++ src/Pages/Flashcard/ReviewCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../../Styles/ReviewCard.css";
+import "../../Styles/reviewCard.css";
 import "../../Styles/GlobalButton.css";
 import { getCurrentUserDetail, getCurrentUserToken } from "../../Auth/auth";
 import { useNavigate } from "react-router-dom";
